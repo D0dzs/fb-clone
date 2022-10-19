d6dd3bcaecb47708fd3177c5a64689e8da9c448f
D0dzs <51715950+D0dzs@users.noreply.github.com>
2022-10-19 20:57:26 +0200

    Error: Using target="_blank" w/o rel="noreferrer"

--- src/components/GithubSrc.tsx
+++ src/components/GithubSrc.tsx
@@ -6,7 +6,7 @@ export default function GithubSrc() {
       className="absolute bottom-8 right-8 text-white bg-black p-2 rounded-full cursor-pointer
     before:content-['SRC'] text-sm before:absolute before:bottom-14 before:right-3 before:translate-y-1 before:opacity-0 hover:before:translate-y-0 hover:before:opacity-100 before:transition-all before:duration-300"
     >
-      <a href="https://github.com/D0dzs/fb-clone" target={"_blank"}>
+      <a href="https://github.com/D0dzs/fb-clone" target={"_blank"} rel="noreferrer">
         <AiOutlineGithub size={32} fill="white" className="mx-auto" />
       </a>
     </div>
